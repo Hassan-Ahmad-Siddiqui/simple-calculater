577b504df45bd3f5a3225b30a4d1cda2ebc98916
Hassan Ahmad Siddiqui <hassanas886@gmail.com>
2024-04-02 00:07:57 +0500

    add

---
+++ main.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Kindly enter your first no: ",
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Kindly enter your second no: ",
+    },
+    {
+        type: "list",
+        name: "operater",
+        choices: ["*", "+", "-", "/"],
+        message: "Select your operater: ",
+    },
+]);
+const { numberOne, numberTwo, operater } = answers;
+if (numberOne && numberTwo && operater) {
+    let result = 0;
+    if (operater === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (operater === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (operater === "/") {
+        result = numberOne / numberTwo;
+    }
+    if (operater === "*") {
+        result = numberOne * numberTwo;
+    }
+    console.log("Your result is :", result);
+}
+else {
+    console.log("Kindly enter valid input");
+}
